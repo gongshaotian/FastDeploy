228d59b4aa29b8f4ccf5065e7c3e391c92b85fd0
gongshaotian <gstian5555@outlook.com>
2026-05-08 20:23:44 +0800

    fix test case

--- tests/operators/test_get_position_ids_and_mask_encoder_batch.py
+++ tests/operators/test_get_position_ids_and_mask_encoder_batch.py
@@ -27,17 +27,17 @@ class TestGetPositionIds(unittest.TestCase):
 
     def test_basic_functionality(self):
         # Test normal case with batch size 2
-        seq_lens_encoder = paddle.to_tensor([3, 2], dtype="int32")
-        seq_lens_decoder = paddle.to_tensor([1, 2], dtype="int32")
+        seq_lens_encoder = paddle.to_tensor([1, 2], dtype="int32")
+        seq_lens_decoder = paddle.to_tensor([3, 2], dtype="int32")
         seq_lens_this_time = paddle.to_tensor([1, 2], dtype="int32")
 
-        total_len = int(seq_lens_encoder.numpy().sum() + seq_lens_this_time.numpy().sum())
+        total_len = int(seq_lens_this_time.numpy().sum())
         position_ids = paddle.zeros([total_len], dtype="int32")
 
         # Call the custom operator
         get_position_ids(seq_lens_encoder, seq_lens_decoder, seq_lens_this_time, position_ids)
 
-        expected_position_ids = np.array([0, 1, 2, 1, 0, 1, 2, 3], dtype=np.int32)
+        expected_position_ids = np.array([3, 2, 3], dtype=np.int32)
 
         # Convert to numpy for comparison
         position_ids_np = position_ids.numpy()
@@ -49,7 +49,7 @@ class TestGetPositionIds(unittest.TestCase):
         # Test case where decoder length is 0
         seq_lens_encoder = paddle.to_tensor([2], dtype="int32")
         seq_lens_decoder = paddle.to_tensor([0], dtype="int32")
-        seq_lens_this_time = paddle.to_tensor([0], dtype="int32")
+        seq_lens_this_time = paddle.to_tensor([2], dtype="int32")
 
         position_ids = paddle.zeros([2], dtype="int32")
 
